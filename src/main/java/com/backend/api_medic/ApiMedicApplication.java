3ad5bc5daa2cd1bb917019ef9bf66f8107929d18
Yorman Rojas <yormanxrojas@gmail.com>
2024-09-03 14:40:10 -0600

    init project

---
+++ src/main/java/com/backend/api_medic/ApiMedicApplication.java
@@ -0,0 +1,13 @@
+package com.backend.api_medic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicApplication.class, args);
+	}
+
+}
