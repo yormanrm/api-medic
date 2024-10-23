7926c1a47cbeca30094589c3a55537f93e3cbff5
Yorman Rojas <yormanxrojas@gmail.com>
2024-10-23 13:18:52 -0600

    Patient model sanitized

--- src/main/java/com/backend/api_medic/domain/model/Patient.java
+++ src/main/java/com/backend/api_medic/domain/model/Patient.java
@@ -1,9 +1,6 @@
 package com.backend.api_medic.domain.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,15 @@ public class Patient {
     private Integer id;
 
     @NotBlank(message = "Full name is required")
+    @Size(
+            min = 2,
+            max = 100,
+            message = "Full name must be between 2 and 100 characters"
+    )
+    @Pattern(
+            regexp = "^[a-zA-Z\\s]+$",
+            message = "Full name can only contain letters and spaces"
+    )
     private String fullName;
 
     @NotNull(message = "Date of birth is required")
@@ -24,9 +30,18 @@ public class Patient {
     private LocalDateTime birthday;
 
     @NotBlank(message = "Address is required")
+    @Size(
+            min = 2,
+            max = 255,
+            message = "Address must be between 2 and 255 characters"
+    )
     private String address;
 
     @NotBlank(message = "Telephone is required")
+    @Pattern(
+            regexp = "^\\+52\\d{10}$",
+            message = "Telephone number must follow the format +52XXXXXXXXXX"
+    )
     private String telephone;
 
     @NotBlank(message = "Email is required")
--- src/main/java/com/backend/api_medic/infrastructure/handler/GlobalExceptionHandler.java
+++ src/main/java/com/backend/api_medic/infrastructure/handler/GlobalExceptionHandler.java
@@ -18,7 +18,7 @@ public class GlobalExceptionHandler {
 
     // Manejo de cualquier otra excepcion inesperada
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponseDTO<Object>> handleGeneralException(Exception ex) {
+    public ResponseEntity<ApiResponseDTO<Object>> handleGeneralExceptions(Exception ex) {
         ApiResponseDTO<Object> response = new ApiResponseDTO<>(
                 500, true, "Unexpected error: " + ex.getMessage(), null);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -26,7 +26,7 @@ public class GlobalExceptionHandler {
 
     // Manejo de exceptions personalizadas
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ApiResponseDTO<Object>> handleCustomException(CustomException ex) {
+    public ResponseEntity<ApiResponseDTO<Object>> handleCustomExceptions(CustomException ex) {
         ApiResponseDTO<Object> response = new ApiResponseDTO<>(
                 -1, true, "Unexpected error: " + ex.getMessage(), null);
         return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
@@ -46,7 +46,7 @@ public class GlobalExceptionHandler {
 
     // Manejo de pacientes duplicados
     @ExceptionHandler(PatientAlreadyExistsException.class)
-    public ResponseEntity<ApiResponseDTO<Object>> handlePatientAlreadyExistsException(PatientAlreadyExistsException ex) {
+    public ResponseEntity<ApiResponseDTO<Object>> handlePatientAlreadyExistsExceptions(PatientAlreadyExistsException ex) {
         ApiResponseDTO<Object> response = new ApiResponseDTO<>(
                 409, true, "Unexpected error: " + ex.getMessage(), null);
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
